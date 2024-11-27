5f19bfc6934d96a2bc60f98daf9ad1a62dbafbc0
Сергей Политаев <ronin@sonikelf.ru>
2024-11-27 23:38:32 +0300

    Add General info fields to the editor

--- src/app/(main)/editor/resume-editor.tsx
+++ src/app/(main)/editor/resume-editor.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
+import GeneralInfoForm from "./form/general-info-form";
 
 export default function ResumeEditor() {
   return (
@@ -14,7 +16,9 @@ export default function ResumeEditor() {
       </header>
       <main className="relative grow">
         <div className="absolute bottom-0 top-0 flex w-full">
-          <div className="w-full md:w-1/2">left</div>
+          <div className="w-full p-3 md:w-1/2">
+            <GeneralInfoForm />
+          </div>
           <div className="grow md:border-r" />
           <div className="hidden w-1/2 md:flex">right</div>
         </div>
