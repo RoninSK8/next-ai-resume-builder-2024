961f37447c5777da0b381fde77fe5190d5f32b58
Сергей Политаев <ronin@sonikelf.ru>
2025-01-06 20:05:14 +0300

    Add information on free plan to premium modal window and fix styles

--- src/components/premium/premium-modal.tsx
+++ src/components/premium/premium-modal.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import { useToast } from "@/hooks/use-toast";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
-import { Check } from "lucide-react";
+import { Check, X } from "lucide-react";
 import { Button } from "../ui/button";
 import usePremiumModal from "@/hooks/use-premium-modal";
 import { createCheckoutSession } from "./actions";
@@ -11,8 +11,13 @@ import { env } from "@/env";
 import { useSubscriptionLevel } from "@/app/(main)/subscription-level-provider";
 import { canPurchasePro, canPurchaseProPlus } from "@/lib/permissions";
 
-const premiumFeatures = ["AI tools", "Up to 3 resumes"];
-const premiumPlusFeatures = ["infinite resumes", "Design customizations"];
+const freeFeatures = ["Add single resume"];
+const premiumFeatures = ["Up to 3 resumes", "AI tools"];
+const premiumPlusFeatures = [
+  "infinite resumes",
+  "AI tools",
+  "Design customizations",
+];
 
 export const PremiumModal: React.FC = () => {
   const { open, setOpen } = usePremiumModal();
@@ -43,16 +48,38 @@ export const PremiumModal: React.FC = () => {
         }
       }}
     >
-      <DialogContent className="max-w-2xl">
+      <DialogContent className="max-w-3xl">
         <DialogHeader>
-          <DialogTitle>AI Resume Builder Premium</DialogTitle>
+          <DialogTitle className="text-center">
+            AI Resume Builder Premium
+          </DialogTitle>
         </DialogHeader>
         <div className="space-y-6">
-          <p>Get a premium subscription to unlock more features.</p>
+          <p className="text-center">
+            Get a premium subscription to unlock more features.
+          </p>
           <div className="flex">
-            <div className="flex w-1/2 flex-col space-y-5">
+            <div className="flex w-1/2 flex-col">
+              <h3 className="text-center text-lg font-bold">Free</h3>
+              <ul className="my-5 list-inside space-y-2">
+                {freeFeatures.map((feature) => (
+                  <li className="flex items-center gap-2" key={feature}>
+                    <Check className="size-4" />
+                    {feature}
+                  </li>
+                ))}
+                {premiumPlusFeatures.slice(1).map((feature) => (
+                  <li className="flex items-center gap-2" key={feature}>
+                    <X className="size-4" />
+                    {feature}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="mx-6 border-l" />
+            <div className="flex w-1/2 flex-col">
               <h3 className="text-center text-lg font-bold">Premium</h3>
-              <ul className="list-inside space-y-2">
+              <ul className="my-5 list-inside space-y-2">
                 {premiumFeatures.map((feature) => (
                   <li className="flex items-center gap-2" key={feature}>
                     <Check className="size-4 text-green-500" />
@@ -67,6 +94,7 @@ export const PremiumModal: React.FC = () => {
                     env.NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_MONTHLY,
                   )
                 }
+                className="mt-auto"
               >
                 {subscriptionLevel !== "free" && (
                   <Check className="size-4 text-green-500" />
@@ -77,11 +105,11 @@ export const PremiumModal: React.FC = () => {
               </Button>
             </div>
             <div className="mx-6 border-l" />
-            <div className="flex w-1/2 flex-col space-y-5">
+            <div className="flex w-1/2 flex-col">
               <h3 className="bg-gradient-to-r from-green-600 to-green-400 bg-clip-text text-center text-lg font-bold text-transparent">
                 Premium Plus
               </h3>
-              <ul className="list-inside space-y-2">
+              <ul className="my-5 list-inside space-y-2">
                 {premiumPlusFeatures.map((feature) => (
                   <li key={feature} className="flex items-center gap-2">
                     <Check className="size-4 text-green-500" />
@@ -97,6 +125,7 @@ export const PremiumModal: React.FC = () => {
                     env.NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_PLUS_MONTHLY,
                   )
                 }
+                className="mt-auto"
               >
                 {subscriptionLevel === "pro_plus" && (
                   <Check className="size-4 text-green-500" />
