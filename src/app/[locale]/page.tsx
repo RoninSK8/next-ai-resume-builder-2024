ab0b8a2be607387a4771781060a628292731497f
Сергей Политаев <ronin@sonikelf.ru>
2025-01-15 21:53:01 +0300

    Add i18n for the home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,8 +3,10 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
 import resumePreview from "@/assets/landing-resume-preview.jpg";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
+  const t = useTranslations("HomePage");
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-6 bg-gray-100 px-5 py-12 text-center text-gray-900 md:flex-row md:text-start lg:gap-12">
       <div className="max-w-prose space-y-3">
@@ -16,25 +18,24 @@ export default function Home() {
           className="mx-auto md:ms-0"
         />
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Create the{" "}
+          {t("create-the")}{" "}
           <span className="inline-block bg-gradient-to-r from-green-600 to-green-400 bg-clip-text text-transparent">
-            Perfect Resume
+            {t("perfect-resume")}
           </span>{" "}
-          in Minutes
+          {t("in-minutes")}
         </h1>
         <p className="text-lg text-gray-500">
-          Our <span className="font-bold">AI resume builder</span> is a simple
-          and easy-to-use tool that allows you to create a professional resume
-          that will help you land your dream job.
+          {t("our")} <span className="font-bold">{t("ai-resume-builder")}</span>{" "}
+          {t("is-a-simple")}
         </p>
         <Button asChild size="lg" variant="premium">
-          <Link href="/resumes">Get started</Link>
+          <Link href="/resumes">{t("get-started")}</Link>
         </Button>
       </div>
       <div>
         <Image
           src={resumePreview}
-          alt="Resume preview"
+          alt={t("resume-preview")}
           width={600}
           className="shadow-md lg:rotate-[1.5deg]"
         />
